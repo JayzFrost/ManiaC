aeb2b1baa3dc4663ccad5ed2def163895feabdb2
JayzFrost <nicolybarte1999@gmail.com>
2020-10-27 00:04:09 +0800

    first commit

---
+++ precache-manifest.2f1b40bc60de4659ccdd58a47f0041e9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff328fee4475e691e84",
+    "url": "css/app.0ba28d3a.css"
+  },
+  {
+    "revision": "9a271aa3edd129de5ca1",
+    "url": "css/chunk-vendors.b78cac67.css"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "37c358f2e73b06d9d1adbd2d4aa529db",
+    "url": "img/trianglify-bw-50@2x.37c358f2.png"
+  },
+  {
+    "revision": "36431213f7b3481266a66daf9cf29031",
+    "url": "index.html"
+  },
+  {
+    "revision": "2ff328fee4475e691e84",
+    "url": "js/app-legacy.0350c5a3.js"
+  },
+  {
+    "revision": "30a9a23df4214598ce53",
+    "url": "js/chunk-2d0c86e3-legacy.c751fc6f.js"
+  },
+  {
+    "revision": "39191848ddbf2d4f5144",
+    "url": "js/chunk-2d0e4caa-legacy.c3470f1e.js"
+  },
+  {
+    "revision": "aadabfb2910999866df0",
+    "url": "js/chunk-2d0ea0d9-legacy.1d7e3dc2.js"
+  },
+  {
+    "revision": "2d5e8ed0434f22f55a58",
+    "url": "js/chunk-2d210970-legacy.bb9f7436.js"
+  },
+  {
+    "revision": "9a271aa3edd129de5ca1",
+    "url": "js/chunk-vendors-legacy.1172a682.js"
+  },
+  {
+    "revision": "db282e7205928c22dbd9",
+    "url": "js/full-page-legacy.62c3eb8e.js"
+  },
+  {
+    "revision": "8ec8172d1103d5318fce7b3b2521291d",
+    "url": "manifest.json"
+  }
+]);
---
+++ precache-manifest.e354a9fcb2e20c2dbc72dd98dce36bd7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f6c8b24d940d06d69e",
+    "url": "css/app.0ba28d3a.css"
+  },
+  {
+    "revision": "104ab70bc1190e37e13f",
+    "url": "css/chunk-vendors.b78cac67.css"
+  },
+  {
+    "revision": "d3ac7e7192388ca270c1ec9ff148da1c",
+    "url": "data-sources/clients.json"
+  },
+  {
+    "revision": "46f4eb48300fd5b3d1601ebe15cb7d43",
+    "url": "data-sources/comments.json"
+  },
+  {
+    "revision": "e1ac77b07129201daa5b1468cff724ab",
+    "url": "data-sources/stuff-updates.json"
+  },
+  {
+    "revision": "0ce9df127bb8158600422cb4aa23efad",
+    "url": "data-sources/updates.json"
+  },
+  {
+    "revision": "7298fabbecbc6f87cf690f717af8652c",
+    "url": "img/justboil-logo.7298fabb.svg"
+  },
+  {
+    "revision": "37c358f2e73b06d9d1adbd2d4aa529db",
+    "url": "img/trianglify-bw-50@2x.37c358f2.png"
+  },
+  {
+    "revision": "8414af4653a8d00cb64086d1124edf99",
+    "url": "index.html"
+  },
+  {
+    "revision": "58f6c8b24d940d06d69e",
+    "url": "js/app.a79b6e82.js"
+  },
+  {
+    "revision": "4ae0bf4a29c4524c84d3",
+    "url": "js/chunk-2d0c86e3.c3c8ba92.js"
+  },
+  {
+    "revision": "d367a555d9d492c9f562",
+    "url": "js/chunk-2d0e4caa.2f1a45e0.js"
+  },
+  {
+    "revision": "47a171a4c0eaba109bf5",
+    "url": "js/chunk-2d0ea0d9.7dc14ce1.js"
+  },
+  {
+    "revision": "b62cf941578915a7d47d",
+    "url": "js/chunk-2d210970.eb9e6fe2.js"
+  },
+  {
+    "revision": "104ab70bc1190e37e13f",
+    "url": "js/chunk-vendors.d025e6e0.js"
+  },
+  {
+    "revision": "452cbd883d4831490d15",
+    "url": "js/full-page.f769f1ca.js"
+  },
+  {
+    "revision": "8ec8172d1103d5318fce7b3b2521291d",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
